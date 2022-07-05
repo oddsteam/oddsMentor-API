5c0d8a3109e2b953fe525d1a10f90fff87a454b5
Jirawat-rackz <Jirawat_ku@kkumail.com>
2022-07-05 17:45:25 +0700

    [porz] check deploy script pwd

--- deploy-script.sh
+++ deploy-script.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 docker login -u ap-southeast-2@OA4R6SQSJDS6O5TPXWUJ -p 092929273c8458b0141bdca0a6475a3f3103eb3f4fa57b4a5405635828bc4c9a ${REGISTRY}
+echo pwd
 docker compose pull
 docker compose down
 docker compose up -d
