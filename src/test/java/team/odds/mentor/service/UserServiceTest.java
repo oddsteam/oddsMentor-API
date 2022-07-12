2ca61f42b966ab8217a99d0b9f6bea39f80b30b9
Chandara-Sin <>
2022-07-12 21:14:13 +0700

    refactor: add get user list and id not found exception test

--- src/test/java/team/odds/mentor/service/UserServiceTest.java
+++ src/test/java/team/odds/mentor/service/UserServiceTest.java
@@ -1,21 +1,19 @@
 package team.odds.mentor.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import team.odds.mentor.model.Expertise;
 import team.odds.mentor.model.User;
 import team.odds.mentor.model.UserResponse;
-import team.odds.mentor.model.mapper.UserMapper;
 import team.odds.mentor.repository.EndorsementRepository;
 import team.odds.mentor.repository.ExpertiseRepository;
 import team.odds.mentor.repository.UserRepository;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -25,67 +23,48 @@ import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
-    @Mock private UserRepository userRepository;
-    @Mock private ExpertiseRepository expertiseRepository;
-    @Mock private EndorsementRepository endorsementRepository;
-    @InjectMocks private UserService userService;
-    List<UserResponse.Expertise> userExpertiseList = Arrays.asList(
-            UserResponse.Expertise.builder()
-                    .id("11")
-                    .skill("java")
-                    .endorsed(10)
-                    .build(),
-            UserResponse.Expertise.builder()
-                    .id("12")
-                    .skill("go")
-                    .endorsed(20)
-                    .build()
-    );
-    List<String> expertiseIdList = userExpertiseList.stream().map(UserResponse.Expertise::getId).toList();
-    List<Expertise> expertiseList = userExpertiseList.stream().map(item->Expertise.builder()
-            .id(item.getId())
-            .skill(item.getSkill())
-            .createdAt(LocalDateTime.now())
-            .updatedAt(LocalDateTime.now())
-            .build()
-    ).toList();
-    User user = User.builder()
-            .id("1234")
-            .firstNameEN("aiyaruch")
-            .lastNameEN("ph")
-            .firstNameTH("อัย")
-            .lastNameTH("ยย")
-            .nickname("ai")
-            .email("aa@aa")
-            .accountType("oddsMember")
-            .biography("Hello World!")
-            .team("molamola")
-            .position("backend dev")
-            .profileImageUrl("gg")
-            .expertise(expertiseIdList)
-            .createdAt(LocalDateTime.now())
-            .updatedAt(LocalDateTime.now())
-            .build();
-    UserResponse userResponse = UserResponse.builder()
-            .id(user.getId())
-            .fullNameEN(user.getFirstNameEN() + " " + user.getLastNameEN())
-            .fullNameTH(user.getFirstNameTH() + " " + user.getLastNameTH())
-            .nickname(user.getNickname())
-            .type(user.getAccountType())
-            .biography(user.getBiography())
-            .team(user.getTeam())
-            .position(user.getPosition())
-            .profileImageUrl(user.getProfileImageUrl())
-            .totalEndorsed(30)
-            .expertise(userExpertiseList)
-            .build();
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private ExpertiseRepository expertiseRepository;
+    @Mock
+    private EndorsementRepository endorsementRepository;
+    @InjectMocks
+    private UserService userService;
+
     @Test
-    void getUser_shouldReturnUserResponse() {
+    void getUser_shouldReturnUserResponseWhenIdExisted() {
         // Arrange
+        var userExpertiseList = Arrays.asList(
+                UserResponse.Expertise.builder()
+                        .id("11")
+                        .skill("java")
+                        .endorsed(10)
+                        .build(),
+                UserResponse.Expertise.builder()
+                        .id("12")
+                        .skill("go")
+                        .endorsed(20)
+                        .build()
+        );
+        var expertiseIdList = userExpertiseList.stream().map(UserResponse.Expertise::getId).toList();
+        var expertiseList = userExpertiseList.stream().map(item -> Expertise.builder()
+                .id(item.getId())
+                .skill(item.getSkill())
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build()
+        ).toList();
+        var user = User.builder()
+                .id("1234")
+                .expertise(expertiseIdList)
+                .build();
         when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
-        when(expertiseRepository.findExpertiseByArrayId(user.getExpertise())).thenReturn(expertiseList);
-        when(endorsementRepository.countEndorsementByUserIdAndExpertiseId(user.getId(),"11")).thenReturn(10);
-        when(endorsementRepository.countEndorsementByUserIdAndExpertiseId(user.getId(),"12")).thenReturn(20);
+        when(expertiseRepository.findExpertiseByIdList(user.getExpertise())).thenReturn(expertiseList);
+        when(endorsementRepository.countEndorsementByUserIdAndExpertiseId(user.getId(), userExpertiseList.get(0).getId()))
+                .thenReturn(userExpertiseList.get(0).getEndorsed());
+        when(endorsementRepository.countEndorsementByUserIdAndExpertiseId(user.getId(), userExpertiseList.get(1).getId()))
+                .thenReturn(userExpertiseList.get(1).getEndorsed());
         // Act
         var userResponse = userService.getUser(user.getId());
         // Assert
@@ -94,16 +73,67 @@ public class UserServiceTest {
         assertThat(userResponse.getTotalEndorsed()).isEqualTo(30);
     }
 
-//    @Test
-//    void buildUserResponse_shouldReturnUserResponse() {
-//         Arrange
-//        when(userService.buildUserResponse(user)).thenReturn(userResponse);
-//        when(expertiseRepository.findExpertiseBy(user.getExpertise())).thenReturn(expertiseList);
-//        when(userService.buildUserExpertise(user.getId(),expertiseList)).thenReturn(userExpertiseList);
-//         Act
-//        var userRes = userService.buildUserResponse(user);
-//        System.out.println(userRes.getExpertise());
-//         Assert
-//        assertThat(userRes.getId()).isEqualTo("1234");
-//    }
+    @Test
+    void getUser_ShouldReturnErrorWhenIdNotFound() {
+        // Arrange
+        var error = new RuntimeException("User not found with this id " + "");
+        doThrow(error).when(userRepository).findById(any());
+        try {
+            // Act
+            userService.getUser("");
+        } catch (RuntimeException exception) {
+            // Assert
+            assertThat(exception.getMessage()).isEqualTo(error.getMessage());
+        }
+    }
+
+    @Test
+    void getUserList_ShouldReturnUserResponseList() {
+        // Arrange
+        var userExpertiseList = Arrays.asList(
+                UserResponse.Expertise.builder()
+                        .id("11")
+                        .skill("java")
+                        .endorsed(10)
+                        .build(),
+                UserResponse.Expertise.builder()
+                        .id("12")
+                        .skill("go")
+                        .endorsed(20)
+                        .build()
+        );
+        var expertiseIdList = userExpertiseList.stream().map(UserResponse.Expertise::getId).toList();
+        var users = Arrays.asList(
+                User.builder()
+                        .id("1234")
+                        .expertise(expertiseIdList)
+                        .build(),
+                User.builder()
+                        .id("4567")
+                        .expertise(expertiseIdList)
+                        .build()
+        );
+        var expertiseList = userExpertiseList.stream().map(item -> Expertise.builder()
+                .id(item.getId())
+                .skill(item.getSkill())
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build()
+        ).toList();
+        when(userRepository.findAll()).thenReturn(users);
+        when(expertiseRepository.findExpertiseByIdList(users.get(0).getExpertise())).thenReturn(expertiseList);
+        when(endorsementRepository.countEndorsementByUserIdAndExpertiseId
+                (users.get(0).getId(), userExpertiseList.get(0).getId()))
+                .thenReturn(userExpertiseList.get(0).getEndorsed());
+        when(endorsementRepository.countEndorsementByUserIdAndExpertiseId
+                (users.get(0).getId(), userExpertiseList.get(1).getId()))
+                .thenReturn(userExpertiseList.get(1).getEndorsed());
+        // Act
+        var userResponse = userService.getUserList();
+        // Assert
+        assertThat(userResponse.get(0).getId()).isEqualTo("1234");
+        assertThat(userResponse.get(1).getId()).isEqualTo("4567");
+        assertThat(userResponse.get(0).getExpertise()).isEqualTo(userExpertiseList);
+        assertThat(userResponse.get(0).getTotalEndorsed()).isEqualTo(30);
+    }
 }
